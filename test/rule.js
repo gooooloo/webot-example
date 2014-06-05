d814d7711cdf3a40be448cea903e64c0ed7cd596
gooooloo <qidu.lin@gmail.com>
2014-06-06 01:16:19 +0800

    try to use a more easy read way.

--- test/rule.js
+++ test/rule.js
@@ -37,50 +37,94 @@ describe('wechat1', function() {
   });
 
   var textReqRes = function(textReq, textInRes, textNotInRes, next) {
+    var info = {
+      sp: 'webot',
+      user: 'client',
+      type: 'text'
+    };
     info.text = textReq;
     sendRequest(info, function(err, json) {
       detect(info, err, json, textInRes, textNotInRes);
       if (next) { next(); }
     });
+  }
+
+  var webot = function(){};
+  webot.test = function(){};
+  webot.test.input = function(text){
+
+    var testCase = function(){};
+
+    testCase.prototype = {
+
+      get output() {
+        return this;
+      },
+
+      match : function (text) {
+        this.args[this.args.length - 1][1] = text;
+        return this;
+      },
+
+      notmatch : function (text) {
+        this.args[this.args.length - 1][2] = text;
+        return this;
+      },
+
+      input : function (text) {
+        var elem = [];
+        elem.push(text);
+        this.args.push(elem);
+        return this;
+      },
+
+      get end() {
+        var args = this.args;
+        return function(done){
+          if (args.length == 1) {
+            textReqRes(args[0], args[1], args[2], done);
+          } else if (args.length > 1) {
+            var funcs = [];
+            for (var i = 0; i < args.length; i++) {
+              var func = function(k) {
+                // have to save 'i' value for later execution,
+                // we need to write it in this way.
+                return function(callback) {
+                  var arg = args[k];
+                  var next = function(){ callback(null); };
+                  textReqRes(arg[0], arg[1], arg[2], next);
+                };
+              }(i);
+              funcs.push(func);
+            }
+
+            async.series(funcs, function(err, result){ done(); });
+          }
+        };
+      }
+    };
+
+    var ret = new testCase();
+    ret.args= [];
+    ret.args.push([]);
+    ret.args[0][0] = text;
+    return ret;
   };
 
   var testTextInput = function(message, textReq, textInRes, textNotInRes) {
-    it(message, function(done) {
-      textReqRes(textReq, textInRes, textNotInRes, done);
-    });
-  };
-
-  var testTextInputSeries = function() {
-    var args = arguments;
-
-    it(args[0], function(done) {
-      var funcs = [];
-      for (var i = 1; i < args.length; i++) {
-        var func = function(k) {
-          // have to save 'i' value for later execution,
-          // we need to write it in this way.
-          return function(callback) {
-            var arg = args[k];
-            var next = function(){ callback(null); };
-            textReqRes(arg[0], arg[1], arg[2], next);
-          };
-        }(i);
-        funcs.push(func);
-      }
-
-      async.series(funcs, function(err, result){ done(); });
-    });
+    it(message, webot.test.input(textReq).output.match(textInRes).notmatch(textNotInRes).end);
   };
 
   //测试文本消息
   describe('text', function() {
+
     //检测more指令
     testTextInput('should return more msg', 'more', /指令/);
 
     testTextInput('should pass multi line yaml', '帮助', /，\n/);
 
     //检测who指令
-    testTextInput('should return who msg', 'who' , /机器人/);
+    testTextInput('should return who msg', 'who', /机器人/);
 
     //检测name指令
     testTextInput('should return name msg', 'I am a mocha tester', /a mocha tester/);
@@ -106,40 +150,53 @@ describe('wechat1', function() {
     testTextInput('should return yaml msg', 'yaml', /这是一个yaml的object配置/);
   });
 
+  describe('qidu', function() {
+    it('should pass guess sex', webot.test
+      .input('你是男人还是女人').output.match(/猜猜看/)
+      .input('哈哈').output.match(/还猜不猜嘛/)
+      .input('男的').output.match(/是的/)
+      .end);
+  });
+
   //测试wait
   describe('wait', function() {
     //检测sex指令
-    testTextInputSeries('should pass guess sex',
-      ['你是男人还是女人' , /猜猜看/],
-      ['哈哈', /还猜不猜嘛/],
-      ['男的', /是的/]);
+    it('should pass guess sex', webot.test
+      .input('你是男人还是女人').output.match(/猜猜看/)
+      .input('哈哈').output.match(/还猜不猜嘛/)
+      .input('男的').output.match(/是的/)
+      .end);
 
     //检测game指令
-    testTextInputSeries('should pass game-no-found',
-      ['game 1', /游戏/],
-      ['2',/再猜/],
-      ['3', /再猜/],
-      ['4', /答案是/]);
+    it('should pass game-no-found', webot.test
+      .input('game 1').output.match(/游戏/)
+      .input('2').output.match(/再猜/)
+      .input('3').output.match(/再猜/)
+      .input('4').output.match(/答案是/)
+      .end);
 
     //检测game指令
-    testTextInputSeries('should return game-found msg',
-      ['game 1', /游戏/],
-      ['2', /再猜/],
-      ['3', /再猜/],
-      ['1', /聪明/]);
+    it('should return game-found msg', webot.test
+      .input('game 1').output.match(/游戏/)
+      .input('2').output.match(/再猜/)
+      .input('3').output.match(/再猜/)
+      .input('1').output.match(/聪明/)
+      .end);
 
     //检测suggest_keyword指令
-    testTextInputSeries('should return keyword correction accepted result.',
-      ['s nde', /拼写错误.*nodejs/],
-      ['y', /百度搜索.*nodejs/]);
+    it('should return keyword correction accepted result.', webot.test
+        .input('s nde').output.match(/拼写错误.*nodejs/)
+        .input('y').output.match(/百度搜索.*nodejs/)
+        .end);
 
     //检测suggest_keyword指令
-    testTextInputSeries('should return refused keyword correction result.',
-      ['s nde', /拼写错误.*nodejs/],
-      ['n', /百度搜索.*nde/]);
+    it('should return refused keyword correction result.', webot.test
+        .input('s nde').output.match(/拼写错误.*nodejs/)
+        .input('n').output.match(/百度搜索.*nde/)
+        .end);
 
     //检测search指令
-    testTextInput('should return search msg', 's javascript' , /百度搜索.*javascript/);
+    testTextInput('should return search msg', 's javascript', /百度搜索.*javascript/);
 
     //检测timeout指令
     it('should pass not timeout', function(done) {
@@ -171,11 +228,12 @@ describe('wechat1', function() {
       });
     });
 
-    testTextInputSeries('should handle list',
-      ['ok webot', /可用指令/],
-      ['2', /请选择人名/],
-      ['3', /请输入/],
-      ['David', /输入了 David/]);
+    it('should handle list', webot.test
+        .input('ok webot').output.match(/可用指令/)
+        .input('2').output.match(/请选择人名/)
+        .input('3').output.match(/请输入/)
+        .input('David').output.match(/输入了 David/)
+        .end);
   });
 
   //测试地理位置
